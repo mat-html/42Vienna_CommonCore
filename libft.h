770ffe206edf4ba3dcdb5e7ef82c68aeb9a2e980
Jovan Matic <jomatic@student.42vienna.com>
2026-04-24 13:57:41 +0200

    #define corrected to # define and stddef.h library added

--- libft.h
+++ libft.h
@@ -6,13 +6,20 @@
 /*   By: jomatic <jomatic@student.42vienna.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/22 14:47:36 by jomatic           #+#    #+#             */
-/*   Updated: 2026/04/23 13:23:51 by jomatic          ###   ########.fr       */
+/*   Updated: 2026/04/24 13:09:00 by jomatic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
-#define LIBFT_H
-	
-int	ft_isalpha(int	i);
+# define LIBFT_H
+
+# include <stddef.h>
+
+int	ft_isalpha(int i);
+int	ft_isdigit(int i);
+int	ft_isalnum(int i);
+int	ft_isascii(int i);
+int	ft_isprint(int i);
+int	ft_strlen(int i);
 
 #endif
